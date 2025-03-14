70f840d08f490e1536469a486fbd2b051ef73295
Ayush Kumar <ayushkumar20701@gmail.com>
2025-03-14 14:37:13 +0530

    local api

--- frontend-rrcd/src/api/couponApi.js
+++ frontend-rrcd/src/api/couponApi.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_URL = "https://round-robin-coupon-distribution.onrender.com/api";
+// const API_URL = "http://localhost:5000/api";
 
 export const getCoupons = async () => {
     try {
